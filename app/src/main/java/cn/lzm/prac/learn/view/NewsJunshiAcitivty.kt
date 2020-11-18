4ee4b1ce12d8034d0c41995c01c24a8dd2ed33bc
lizm1103@163.com <wslzm19891103>
2020-11-18 16:12:37 +0800

    添加kotlin类

---
+++ app/src/main/java/cn/lzm/prac/learn/view/NewsJunshiAcitivty.kt
@@ -0,0 +1,13 @@
+package cn.lzm.prac.learn.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cn.lzm.prac.learn.R
+
+class NewsJunshiAcitivty : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_junshi)
+    }
+}
